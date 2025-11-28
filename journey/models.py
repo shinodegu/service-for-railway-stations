0b408566403124f84fc8c530598454f647224465
Ihor <andreiunderwhat@mail.com>
2025-11-28 16:28:50 +0100

    feat(models): add domain models for railway station service
    Added models according to project schema:
    - Crew
    - Station
    - Route
    - TrainType
    - Train
    - Journey
    - Order
    - Ticket
    Included all relations and constraints.
    add initial migrations for Journey models

--- journey/models.py
+++ journey/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from service_for_railway_stations import settings
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Station, on_delete=models.CASCADE,
+                               related_name="source")
+    destination = models.ForeignKey(Station, on_delete=models.CASCADE,
+                                    related_name="destination")
+    distance = models.IntegerField()
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(TrainType, on_delete=models.CASCADE)
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    train = models.ForeignKey(Train, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew, related_name="journeys")
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(Journey, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
