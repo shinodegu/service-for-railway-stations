cd075fcdcef725b0a2c7a8560e40d92b9fb0d3f7
Ihor <andreiunderwhat@mail.com>
2025-11-29 21:38:27 +0100

    feat(journey): added basic viewsets for each model

--- journey/views.py
+++ journey/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from journey.models import (Crew, Station,
+                            Route, TrainType,
+                            Train, Journey,
+                            Order, Ticket)
+from rest_framework import viewsets
+from journey.serializers import (
+    CrewSerializer,
+    StationSerializer,
+    RouteSerializer,
+    TrainTypeSerializer,
+    TrainSerializer,
+    JourneySerializer,
+    OrderSerializer,
+    TicketSerializer,
+)
 
-# Create your views here.
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
