349899810175da018be91ef72e2c76cd792f5f08
Ihor <andreiunderwhat@mail.com>
2025-11-29 21:47:42 +0100

    feat(api): add routing for journey application

--- service_for_railway_stations/urls.py
+++ service_for_railway_stations/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("api/journey/", include("journey.urls", namespace="journey")),
+    path("api/journey/", include("journey.urls", namespace="journey")),
     path("api/user/", include("user.urls", namespace="user")),
 ]
